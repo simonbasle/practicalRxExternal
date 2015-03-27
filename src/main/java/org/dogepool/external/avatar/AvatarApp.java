91d47ce51b339aee1471f9e087d3713c21378756
Simon Baslé <simon.basle@gmail.com>
2015-03-27 18:42:37 +0100

    avatar api serves avatar info on /{id}, with images urls

--- src/main/java/org/dogepool/external/avatar/AvatarApp.java
+++ src/main/java/org/dogepool/external/avatar/AvatarApp.java
@@ -2,6 +2,8 @@ package org.dogepool.external.avatar;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,8 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/avatar")
 public class AvatarApp {
 
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map avatarInfo(@PathVariable long id, HttpServletRequest request) {
+        String baseUrl = request.getRequestURL().toString();
+
+        Map<String, String> json = new HashMap<String, String>(4);
+        json.put("user", "" +id);
+        json.put("email", "user." + id + "@avatarservice.com");
+        json.put("large", baseUrl + "/large");
+        json.put("small", baseUrl + "/small");
+        return json;
+    }
+
     @ResponseBody
-    @RequestMapping(value = {"{id}", "{id}/small"}, method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+    @RequestMapping(value = {"{id}/large", "{id}/small"}, method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
     public byte[] avatar(@PathVariable long id, HttpServletRequest request) throws IOException {
         int avatarNumber = (int) id % 8;
         if (avatarNumber == 0)
